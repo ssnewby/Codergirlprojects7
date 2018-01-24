6e3069afb61c274141000adccd146403d11268b8
ssnewby <35549468+ssnewby@users.noreply.github.com>
2018-01-24 05:34:22 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.scanner
+        public class grocery
+        {}
+        public static void main(String[] args)
+        {}
+        Scanner keyboard = new Scanner(System.in);
+String name;
+System.out.print ("Please list three items on your grocery shopping list");
+System.out.printIn();
+System.out.print("Item 1?");
+String item1 = keyboard.nextLine(oranges);
+System.out.print("Item 2?");
+String item2 = keyboard.nextLine(potatoes);
+System.out.print("Item3?");
+String item3 = keyboard.nextLine(salad);
+
+System.out.print("Now enter the quantity of each item");
+System.out.printIn();
+System.out.print("How many" + oranges + "?" );
+int quantity1 = keyboard.nextInt(2);
+System.out.print("How many" + potatoes + "?");
+int quantity2 = keyboard.nextInt(3);
+System.out.print("How many" + salad + "?");
+int quantity3 = keyboard.nextInt(1);
+
+System.out.print("Now, please enter the price of each item.");
+System.out.printIn();
+System.out.print("How much does one" + oranges + "cost?");
+float cost1 = keyboard.nextFloat(1.00);
+System.out.print("How much does one" + potatoes + "cost?");
+float cost2 = keyboard.nextFloat(2.00);
+System.out.print("How much does one" + salad + "cost?" );
+float cost3 = keyboard.nextFloat(3.00);
+
+System.out.print("Calculating your grocery bill");
+System.out.printIn();
+float result1 = (2 = 1.00);
+float result2 = (3 = 2.00);
+float result3 = (1 = 3.00);
+float resulttotal = 2.00 + 6.00 + 3.00;
+System.out.print(" Your total cost is" + 11.00);
+
+
+
+
